da6ea8adc6d9d697a9e154cd2b08c1186cd0d3b7
C.W. Betts <computers57@hotmail.com>
2021-02-10 15:33:21 -0700

    Move config of mt32 code to its own function.

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -49,6 +49,9 @@
 //--Added 2012-10-19 by Alun Bestor to allow parallel port emulation
 #include "parport.h"
 //--End of modifications
+//--Added 2021-02-10 by C.W. Betts to allow using our custom MT-32 emulation
+#include "BXMIDIConfig.hpp"
+//--End of modifications
 #include "pci_bus.h"
 #include "midi.h"
 #include "hardware.h"
@@ -657,10 +660,14 @@ void DOSBOX_Init(void) {
 	        "'fluidsynth', to use the built-in MIDI synthesizer. See the\n"
 	        "       [fluidsynth] section for detailed configuration.\n"
 #endif
-#if C_MT32EMU
+					  //--Disabled 2021-02-10 by C.W. Betts: We handle our own MT32 emulation.
+					  //#if C_MT32EMU
+					  //--End of modifications
 	        "'mt32', to use the built-in Roland MT-32 synthesizer.\n"
 	        "       See the [mt32] section for detailed configuration.\n"
-#endif
+					  //--Disabled 2021-02-10 by C.W. Betts: We handle our own MT32 emulation.
+					  //#endif
+					  //--End of modifications
 	        "'auto', to use the first working external MIDI player. This\n"
 	        "       might be a software synthesizer or physical device.");
 
@@ -669,7 +676,9 @@ void DOSBOX_Init(void) {
 	        "Configuration options for the selected MIDI interface.\n"
 	        "This is usually the id or name of the MIDI synthesizer you want\n"
 	        "to use (find the id/name with DOS command 'mixer /listmidi').\n"
-#if (C_FLUIDSYNTH == 1 || C_MT32EMU == 1)
+					  //--Modified 2021-02-10 by C.W. Betts: We handle our own MT32 emulation.
+#if (C_FLUIDSYNTH == 1 || /*C_MT32EMU == 1*/ 1)
+					  //--End of modifications
 	        "- This option has no effect when using the built-in synthesizers\n"
 	        "  (mididevice = fluidsynth or mt32).\n"
 #endif
@@ -695,56 +704,13 @@ void DOSBOX_Init(void) {
 #if C_FLUIDSYNTH
 	FLUID_AddConfigSection(control);
 #endif
-
-	const char *mt32ReverseStereo[] = {"off", "on",0};
-	Pstring = secprop->Add_string("mt32ReverseStereo",Property::Changeable::WhenIdle,"off");
-	Pstring->Set_values(mt32ReverseStereo);
-	Pstring->Set_help("Reverse stereo channels for MT-32 output");
     
-	const char *mt32DACModes[] = {"0", "1", "2", "3", "auto",0};
-	Pstring = secprop->Add_string("mt32DAC",Property::Changeable::WhenIdle,"auto");
-	Pstring->Set_values(mt32DACModes);
-	Pstring->Set_help("MT-32 DAC input mode\n"
-                      "Nice = 0 - default\n"
-                      "Produces samples at double the volume, without tricks.\n"
-                      "Higher quality than the real devices\n\n"
-                      
-                      "Pure = 1\n"
-                      "Produces samples that exactly match the bits output from the emulated LA32.\n"
-                      "Nicer overdrive characteristics than the DAC hacks (it simply clips samples within range)\n"
-                      "Much less likely to overdrive than any other mode.\n"
-                      "Half the volume of any of the other modes, meaning its volume relative to the reverb\n"
-                      "output when mixed together directly will sound wrong. So, reverb level must be lowered.\n"
-                      "Perfect for developers while debugging :)\n\n"
-                      
-                      "GENERATION1 = 2\n"
-                      "Re-orders the LA32 output bits as in early generation MT-32s (according to Wikipedia).\n"
-                      "Bit order at DAC (where each number represents the original LA32 output bit number, and XX means the bit is always low):\n"
-                      "15 13 12 11 10 09 08 07 06 05 04 03 02 01 00 XX\n\n"
-                      
-                      "GENERATION2 = 3\n"
-                      "Re-orders the LA32 output bits as in later geneerations (personally confirmed on my CM-32L - KG).\n"
-                      "Bit order at DAC (where each number represents the original LA32 output bit number):\n"
-                      "15 13 12 11 10 09 08 07 06 05 04 03 02 01 00 14\n\n");
-	const char *mt32reverbModes[] = {"0", "1", "2", "3", "auto",0};
-	Pstring = secprop->Add_string("mt32reverb.mode",Property::Changeable::WhenIdle,"auto");
-	Pstring->Set_values(mt32reverbModes);
-	Pstring->Set_help("MT-32 reverb mode");
-
-	const char *mt32reverbTimes[] = {"0", "1", "2", "3", "4", "5", "6", "7",0};
-	Pint = secprop->Add_int("mt32reverb.time",Property::Changeable::WhenIdle,5);
-	Pint->Set_values(mt32reverbTimes);
-	Pint->Set_help("MT-32 reverb time");
-
-	const char *mt32reverbLevels[] = {"0", "1", "2", "3", "4", "5", "6", "7",0};
-	Pint = secprop->Add_int("mt32reverb.level",Property::Changeable::WhenIdle,3);
-	Pint->Set_values(mt32reverbLevels);
-	Pint->Set_help("MT-32 reverb level");
-    
-	//--Disabled 2020-12-31 by C.W. Betts: We handle our own MT32 emulation.
-	//#if C_MT32EMU
-	//	MT32_AddConfigSection(control);
-	//#endif
+#if C_MT32EMU
+	MT32_AddConfigSection(control);
+#endif
+	
+	//--Added 2021-02-10 by C.W. Betts to allow using our custom MT-32 emulation
+	BXMIDIMT32_AddConfigSection(control);
 	//--End of modifications
 
 #if C_DEBUG
